cf784d9d70e8efe80b1de70d62d46d7c8abbbd82
Susanna <susanna.huhtanen@basilicom.de>
2015-03-26 12:10:16 +0100

    whoopsie

--- testBlueprint/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/cannonerd/privateProjects/testExample/testBlueprint/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/cannonerd/privateProjects/testExample/testBlueprint/platforms/android/CordovaLib/AndroidManifest.xml \
--- testBlueprint/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/cannonerd/privateProjects/testExample/testBlueprint/platforms/android/ant-build/AndroidManifest.xml : \
-/home/cannonerd/privateProjects/testExample/testBlueprint/platforms/android/AndroidManifest.xml \
